37bd28e11799a92bd29bac156ed45d6b4f4edb12
almostcrimes <imallhookedup@gmail.com>
2012-04-08 13:47:26 -0400

    Added src/ and plugin.yml

---
+++ src/me/almostcrimes/ACFishing/CatchFish.java
@@ -0,0 +1,59 @@
+package me.almostcrimes.ACFishing;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+
+public class CatchFish implements Listener 
+{
+
+	private boolean caughtFish = false;
+
+	public void setCaughtFish(boolean caughtFish) 
+	{
+		this.caughtFish = caughtFish;
+	}
+
+	/*
+	 * The following method was written with help from Edward Hand.
+	 */
+
+	@EventHandler
+	public void onPlayerPickupItem(PlayerPickupItemEvent event) 
+	{
+		if (event.getItem().getItemStack().getTypeId() == 349) 
+		{
+			caughtFish = true;
+			new CatchReset(2); // resets Catch fish value.
+		}
+	}
+
+	public boolean isCaughtFish() 
+	{
+		return caughtFish;
+	}
+
+	public class CatchReset 
+	{
+		Timer timer;
+
+		public CatchReset(int seconds) 
+		{
+			timer = new Timer();
+			timer.schedule(new RemindTask(), seconds * 1000);
+		}
+
+		// Declaration of the timer and its method.
+		class RemindTask extends TimerTask 
+		{
+			public void run() 
+			{
+				setCaughtFish(false);
+				timer.cancel();
+			}
+		}
+	}
+}
---
+++ src/me/almostcrimes/ACFishing/DropFish.java
@@ -0,0 +1,57 @@
+package me.almostcrimes.ACFishing;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+
+public class DropFish implements Listener 
+{
+
+	private boolean dropedFish = false;
+
+	// method for the reset timer to reset dropedFish to false.
+	public void setDropedFish(boolean dropedFish) 
+	{
+		this.dropedFish = dropedFish;
+	}
+
+	@EventHandler
+	public void onPlayerDropItem(PlayerDropItemEvent event) 
+	{
+			if (event.getItemDrop().getItemStack().getTypeId() == 349) 
+			{
+				new DropReset(60); // resets dropedFish to false after 60 seconds.
+				dropedFish = true;
+			}
+			
+		}
+
+	public boolean isDropedFish() 
+	{
+		return dropedFish;
+	}
+
+	// Declaration of a Timer, to reset the value dropedFish
+	public class DropReset 
+	{
+		Timer timer;
+
+		public DropReset(int seconds) 
+		{
+			timer = new Timer();
+			timer.schedule(new RemindTask(), seconds * 1000);
+		}
+
+		// Declaration of the timer and its method.
+		class RemindTask extends TimerTask 
+		{
+			public void run() 
+			{
+				setDropedFish(false);
+			}
+		}
+	}
+}
